364db7d02417f4fb23b9fd58c3460b12e93233c8
Christopher Penkin <chris.penkin@gmail.com>
2015-12-03 19:37:45 +0200

    Removed a DragonTransactionTestCase for custom transaction management.

--- swampdragon/testing/dragon_testcase.py
+++ swampdragon/testing/dragon_testcase.py
@@ -3,7 +3,7 @@ from swampdragon import discover_routes
 from swampdragon import route_handler
 from swampdragon.pubsub_providers.subscriber_factory import get_subscription_provider
 from swampdragon.connections.mock_connection import TestConnection
-from django.test import TestCase, TransactionTestCase
+from django.test import TestCase
 from django.conf import settings
 try:
     from importlib import import_module
@@ -20,41 +20,6 @@ pub_sub = get_subscription_provider()
 class DragonTestCase(TestCase):
     def __init__(self, methodName='runTest'):
         super(DragonTestCase, self).__init__(methodName)
-
-        pub_sub._channels = []
-        pub_sub._subscribers = {}
-        self.connection = TestConnection()
-        self.urls = discover_routes()
-
-    def tearDown(self):
-        route_handler.registered_handlers = {}
-        pub_sub._channels = []
-        pub_sub._subscribers = {}
-
-    def _load_app(self):
-        routers = []
-        module_name, cls_name = settings.SWAMP_DRAGON_CONNECTION[0].rsplit('.', 1)
-        module = import_module(module_name)
-        cls = getattr(module, cls_name)
-        channel = settings.SWAMP_DRAGON_CONNECTION[1]
-        routers.append(SockJSRouter(cls, channel))
-        print('Channel {}'.format(channel))
-
-        app_settings = {
-            'debug': settings.DEBUG,
-        }
-
-        urls = discover_routes()
-        for router in routers:
-            urls += router.urls
-
-        app = web.Application(urls, **app_settings)
-        return app
-
-
-class DragonTransactionTestCase(TransactionTestCase):
-    def __init__(self, methodName='runTest'):
-        super(TransactionTestCase, self).__init__(methodName)
 
         pub_sub._channels = []
         pub_sub._subscribers = {}
